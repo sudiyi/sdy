8d763f9abaaa6b114d243bdd72631ce847fdd344
Lynn Deng <beednln@163.com>
2017-12-08 22:29:16 +0800

    sdy_tingyun_gin

---
+++ gin/sdy_tingyun_gin/README.md
@@ -0,0 +1,30 @@
+## Synopsis
+```
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sudiyi/sdy/gin/sdy_tingyun_gin"
+	tingyun "github.com/TingYunAPM/go"
+)
+
+// Your gin handler in a controller
+func Fun(c *gin.Context) {
+	// Creates an action wrapping the Tingyun action of the request
+	action := sdy_tingyun_gin.NewAction(c)
+	out := gin.H{}
+	// Runs a Tingyun component in name "root", with function ServeA and its arguments
+	action.Run("root", nil, ServeA, 1, out)
+	c.JSON(200, out)
+}
+
+// Tingyun component function:
+// The 1st argument must be an action that you created in handler.
+// The 2nd argument must be a component of the caller of this function.
+// And the rest are the arguments you passed in action.Run
+func ServeA(action sdy_tingyun_gin.IAction, component *tingyun.Component, a int, out gin.H) {
+	action.Run("sub", component, ServeB, a, 2, out)
+}
+
+func ServeB(action sdy_tingyun_gin.IAction, parent *tingyun.Component, a, b int, out gin.H) {
+	out["sum"] = a + b
+}
+```
---
+++ gin/sdy_tingyun_gin/sdy_tingyun_gin.go
@@ -0,0 +1,47 @@
+package sdy_tingyun_gin
+
+import (
+	"reflect"
+
+	tingyun "github.com/TingYunAPM/go"
+	tingyun_gin "github.com/TingYunAPM/go/framework/gin"
+	"github.com/gin-gonic/gin"
+)
+
+type IAction interface {
+	Run(name string, parent *tingyun.Component, f interface{}, args ...interface{})
+}
+
+type Action struct {
+	action *tingyun.Action
+}
+
+func NewAction(c *gin.Context) IAction {
+	return &Action{tingyun_gin.FindAction(c)}
+}
+
+const argsBeginIndex = 2
+
+func (a *Action) Run(name string, parent *tingyun.Component, f interface{}, args ...interface{}) {
+	vf := reflect.ValueOf(f)
+	if reflect.Func != vf.Kind() {
+		return
+	}
+
+	vfArgs := make([]reflect.Value, argsBeginIndex+len(args))
+	vfArgs[0] = reflect.ValueOf(a)
+	for i, arg := range args {
+		vfArgs[argsBeginIndex+i] = reflect.ValueOf(arg)
+	}
+
+	var component *tingyun.Component
+	if nil != parent {
+		component = parent.CreateComponent(name)
+	} else {
+		component = a.action.CreateComponent(name)
+	}
+	defer component.Finish()
+
+	vfArgs[1] = reflect.ValueOf(component)
+	vf.Call(vfArgs)
+}
