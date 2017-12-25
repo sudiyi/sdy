da781b8c5b040822fd11deb2aa4a980f9ae1fc37
jhy <jhy_0326@163.com>
2017-12-25 15:22:35 +0800

    [ks] add kafka wrapper content

---
+++ aliware/kafka/wrappercontent.go
@@ -0,0 +1,38 @@
+package kafka
+
+import (
+	"fmt"
+	"github.com/satori/go.uuid"
+	"time"
+)
+
+type Wrapper struct {
+	Header HeaderWrapper          `json:"header"`
+	Body   map[string]interface{} `json:"body"`
+}
+
+type HeaderWrapper struct {
+	Timestamp int64  `json:"timestamp"`
+	Type      int    `json:"type"`
+	Uid       string `json:"uid"`
+	Timeout   int32  `json:"timeout"`
+	UserAgent string `json:"user_agent"`
+}
+
+func ContentWrapper(kafkaType int, userAgent string, content map[string]interface{}) *Wrapper {
+	return &Wrapper{
+		Header: *headerWrapper(kafkaType, userAgent),
+		Body:   content,
+	}
+}
+
+func headerWrapper(kafkaType int, userAgent string) *HeaderWrapper {
+	uid := fmt.Sprintf("%s", uuid.NewV4())
+	return &HeaderWrapper{
+		Timestamp: time.Now().UnixNano() / 1000 / 1000,
+		Type:      kafkaType,
+		Uid:       uid,
+		UserAgent: userAgent,
+		Timeout:   3000,
+	}
+}
