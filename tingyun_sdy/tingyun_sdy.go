9f0731a3256455cb22c54257725b4b11c67cdb92
Lynn Deng <beednln@163.com>
2018-01-11 18:07:19 +0800

    advance tingyun sdy

--- tingyun_sdy/tingyun_sdy.go
+++ tingyun_sdy/tingyun_sdy.go
@@ -7,37 +7,30 @@ import (
 	tingyun "github.com/TingYunAPM/go"
 )
 
-type Action struct {
-	*tingyun.Action
-}
-
-const argsBeginIndex = 2
-
-func (a *Action) RunWithName(name string, parent *tingyun.Component, f interface{}, args ...interface{}) {
+func RunWithName(name string, parent *tingyun.Component, action *tingyun.Action, f interface{}, args ...interface{}) {
 	vf := reflect.ValueOf(f)
 	if reflect.Func != vf.Kind() {
 		return
 	}
 
-	vfArgs := make([]reflect.Value, argsBeginIndex+len(args))
-	vfArgs[0] = reflect.ValueOf(a)
+	vfArgs := make([]reflect.Value, 1+len(args))
 	for i, arg := range args {
-		vfArgs[argsBeginIndex+i] = reflect.ValueOf(arg)
+		vfArgs[i+1] = reflect.ValueOf(arg)
 	}
 
 	var component *tingyun.Component
 	if nil != parent {
 		component = parent.CreateComponent(name)
 	} else {
-		component = a.CreateComponent(name)
+		component = action.CreateComponent(name)
 	}
 	defer component.Finish()
 
-	vfArgs[1] = reflect.ValueOf(component)
+	vfArgs[0] = reflect.ValueOf(component)
 	vf.Call(vfArgs)
 }
 
-func (a *Action) Run(parent *tingyun.Component, f interface{}, args ...interface{}) {
+func Run(parent *tingyun.Component, action *tingyun.Action, f interface{}, args ...interface{}) {
 	name := runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name()
-	a.RunWithName(name, parent, f, args...)
+	RunWithName(name, parent, action, f, args...)
 }
